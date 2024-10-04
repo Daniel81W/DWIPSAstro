ce593ed3a9bd056b4aff43580f8fb253a0d07629
Daniel81W <it@dan-web.de>
2024-10-03 22:40:27 -0400

    #

--- DWIPSSun/module.php
+++ DWIPSSun/module.php
@@ -370,7 +370,7 @@ class DWIPSSun extends IPSModule
         //$this->SetValue("solarirradianceground", $solarirradiancespace * 0.75 * sin(deg2rad($sunelevation)));
         //$this->SetValue("solarirradiancepvcollector", $solarirradiancespace * 0.75 * (cos(deg2rad($sunelevation)) * cos(deg2rad($solarAzimut - 183)) * sin(deg2rad(39)) + sin(deg2rad($sunelevation)) * cos(deg2rad(39))));
 
-        ASTROMOON::PeriodicTermsForTheMoon();
+        IPS_LogMessage("MOON2", var_dump(ASTROMOON::PeriodicTermsForTheMoon()));
     }
 
     public function SurfacesIncidenceAngle($orientation, $slope){
