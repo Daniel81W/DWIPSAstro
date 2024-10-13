738014a4ecbc0aef54db33cbe664a73522afdf72
Daniel81W <it@dan-web.de>
2024-10-13 13:34:31 -0400

    #

--- DWIPSSun/module.php
+++ DWIPSSun/module.php
@@ -176,7 +176,6 @@ class DWIPSSun extends IPSModule
         /////////Testumgebung
 
         $this->RegisterAttributeString("TestCalc_DateTime", "{'year':2024,'month':7,'day':1,'hour':12,'minute':0,'second':0}");
-        $this->WriteStringAttribute("TestCalc_DateTime", "{'year':2024,'month':7,'day':1,'hour':12,'minute':0,'second':0}");
         $this->RegisterAttributeFloat("TestCalc_Lat", 0);
         $this->RegisterAttributeFloat("TestCalc_Long", 0);
         $this->RegisterAttributeFloat("TestCalc_Elevation", 0);
@@ -273,6 +272,7 @@ class DWIPSSun extends IPSModule
     {
 
         $this->WriteAttributeString($att, $val);
+        $this->LogMessage("SUN", $val);
     }
 
     public function Update()
