312f8b9a1f73710a562482f3335f7320cb410bb1
Daniel81W <it@dan-web.de>
2024-10-13 13:29:44 -0400

    #

--- DWIPSSun/module.php
+++ DWIPSSun/module.php
@@ -174,7 +174,7 @@ class DWIPSSun extends IPSModule
 
 
         /////////Testumgebung
-        $this->RegisterAttributeString("TestCalc_DateTime", "");
+        $this->RegisterAttributeString("TestCalc_DateTime", "{'year':2024,'month':7,'day':1,'hour':12,'minute':0,'second':0}");
         $this->RegisterAttributeFloat("TestCalc_Lat", 0);
         $this->RegisterAttributeFloat("TestCalc_Long", 0);
         $this->RegisterAttributeFloat("TestCalc_Elevation", 0);
